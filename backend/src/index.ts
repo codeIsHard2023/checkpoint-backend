dfe20084ab8c872268ca4b93ba064f562e7abd49
Vladyslav Tezyk <vlad.tezyk.pro@gmail.com>
2025-04-14 09:39:38 +0200

    config: add datasource

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,55 +1,25 @@
 import "reflect-metadata";
+import { buildSchema } from "type-graphql";
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
+import { datasource } from "./datasource";
 
 
 const port: number = 3000;
 
-const typeDefs = `#graphql
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
-  }
-
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
-  type Query {
-    books: [Book]
-  }
-`;
-
-const books = [
-  {
-    title: 'The Awakening',
-    author: 'Kate Chopin',
-  },
-  {
-    title: 'City of Glass',
-    author: 'Paul Auster',
-  },
-];
-
-const resolvers = {
-  Query: {
-    books: () => books,
-  },
-};
-
-async function initialize(){
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
+async function initialize() {
+  await datasource.initialize(); 
+  console.log("Datasource is connected");
+  const schema = await buildSchema({
+    resolvers: [],
   });
-  
+
+  const server = new ApolloServer({ schema });
+
   const { url } = await startStandaloneServer(server, {
-    listen: { port },
+    listen: { port }
   });
-  
-  console.log(`🚀  Server ready at: ${url}`);
+  console.log(`GraphQL server ready at ${url} 🚀`);
 }
 
-initialize()
+initialize();
